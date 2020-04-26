049f6cdad28318eb0dae36ee4313e32b83d5b4a6
liuhang <mliuhang@163.com>
2020-04-26 09:23:12 +0800

    初始化

---
+++ ModuleB/Classes/ModuleBViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ModuleBViewController.swift
+//  ModuleB
+//
+//  Created by mini on 2020/4/26.
+//
+
+import UIKit
+
+open class ModuleBViewController: UIViewController {
+
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    open func moduleBRun() {
+        
+    }
+
+
+}
